fe13d23403485603f155c4070e6d2b9e486509e0
jQuery <info@zavox.nl>
2015-08-14 03:04:05 +0200

    Working on this later

---
+++ ElTristana/ElTristana/MenuInit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+
+
+namespace ElTristana
+{
+
+    public class MenuInit
+    {
+
+        public static Menu Menu;
+
+        public static void Initialize()
+        {
+            Menu = new Menu("ElTristana WIP", "menu", true);
+
+            var orbwalkerMenu = new Menu("Orbwalker", "orbwalker");
+            Tristana.Orbwalker = new Orbwalking.Orbwalker(orbwalkerMenu);
+
+            Menu.AddSubMenu(orbwalkerMenu);
+
+            var targetSelector = new Menu("Target Selector", "TargetSelector");
+            TargetSelector.AddToMenu(targetSelector);
+
+            Menu.AddSubMenu(targetSelector);
+
+            var cMenu = new Menu("Combo", "Combo");
+            cMenu.AddItem(new MenuItem("ElTristana.Combo.Q", "Use Q").SetValue(true));
+            cMenu.AddItem(new MenuItem("ElTristana.Combo.E", "Use E").SetValue(true));
+            cMenu.AddItem(new MenuItem("ElTristana.Combo.R", "Use R").SetValue(true));
+
+            Menu.AddSubMenu(cMenu);
+
+            var credits = new Menu("Credits", "jQuery");
+            credits.AddItem(new MenuItem("ElTristana.Paypal", "if you would like to donate via paypal:"));
+            credits.AddItem(new MenuItem("ElTristana.Email", "info@zavox.nl"));
+            Menu.AddSubMenu(credits);
+
+            Menu.AddItem(new MenuItem("422442fsaafs4242f", ""));
+
+            Menu.AddItem(new MenuItem("422442fsaafsf", ($"ElTristana by jQuery v{Tristana.ScriptVersion}")));
+            Menu.AddItem(new MenuItem("fsasfafsfsafsa", "Made By jQuery"));
+
+            Menu.AddToMainMenu();
+
+            Console.WriteLine("Menu Loaded");
+        }
+    }
+}
---
+++ ElTristana/ElTristana/Tristana.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using LeagueSharp.Common.Data;
+using ItemData = LeagueSharp.Common.Data.ItemData;
+
+
+namespace ElTristana
+{
+
+    #region 
+
+    internal enum Spells
+    {
+        Q,
+
+        W,
+
+        E,
+
+        R
+    }
+
+    #endregion
+
+
+    internal static class Tristana
+    {
+        #region 
+
+        public static String ScriptVersion
+        {
+            get
+            {
+                return typeof(Tristana).Assembly.GetName().Version.ToString();
+
+            }
+        }
+
+        private static Obj_AI_Hero Player
+        {
+            get
+            {
+                return ObjectManager.Player;
+
+            }
+        }
+
+        #endregion
+
+
+        public static Orbwalking.Orbwalker Orbwalker;
+        public static Dictionary<Spells, Spell> spells = new Dictionary<Spells, Spell>()
+        {
+            { Spells.Q, new Spell(SpellSlot.Q, 0)},
+            { Spells.W, new Spell(SpellSlot.W, 900)},
+            { Spells.E, new Spell(SpellSlot.E, 630)},
+            { Spells.R, new Spell(SpellSlot.R, 630)},
+        };
+
+
+        #region 
+
+        public static void OnLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName != "Tristana") return;
+
+            Console.WriteLine("Injected");
+            Notifications.AddNotification($"ElTristana by jQuery v{ScriptVersion}", 8000);
+
+            try
+            {
+                MenuInit.Initialize();
+                Game.OnUpdate += OnUpdate;
+                //Drawing.OnDraw += Drawings.Drawing_OnDraw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: '{0}'", e);
+            }
+
+        }
+
+        #endregion
+
+        #region OnUpdate    
+
+        private static void OnUpdate(EventArgs args)
+        {
+            if (Player.IsDead) return;
+
+            try
+            {
+                switch (Orbwalker.ActiveMode)
+                {
+                    case Orbwalking.OrbwalkingMode.Combo:
+                        OnCombo();
+                        break;
+                    case Orbwalking.OrbwalkingMode.LaneClear:
+                        break;
+                    case Orbwalking.OrbwalkingMode.Mixed:
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: '{0}'", e);
+            }
+        }
+
+        #endregion
+
+        #region 
+
+        private static void OnCombo()
+        {
+            var target = TargetSelector.GetTarget(spells[Spells.E].Range, TargetSelector.DamageType.Physical);
+            if (target == null || !target.IsValidTarget())
+                return;
+
+            if (spells[Spells.Q].IsReady() && IsActive("ElTristana.Combo.Q"))
+            {
+                
+            }
+        }
+
+        #endregion
+
+        #region 
+
+        private static bool IsActive(string menuItem)
+        {
+            return MenuInit.Menu.Item(menuItem).GetValue<bool>();
+        }
+
+        #endregion
+
+        #region 
+
+        public static bool IsECharged(this Obj_AI_Hero target)
+        {
+            return target.HasBuff("tristanaecharge");
+        }
+
+        #endregion
+
+    }
+}
