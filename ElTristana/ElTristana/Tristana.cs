1c8274520b484e7fdfe88d79fcf36822af2ef725
jQuery <info@zavox.nl>
2015-08-24 17:55:27 +0200

    kden

--- ElTristana/ElTristana/Tristana.cs
+++ ElTristana/ElTristana/Tristana.cs
@@ -158,26 +158,26 @@ namespace ElTristana
                 {
                     Console.WriteLine("E Charged");
                     //Credits to ScienceArk for the - 2 * target.Level broscience 
-                    if (IsBusterShotable(target) - 2 * target.Level > target.Health) 
+                    if (IsBusterShotable(target) - 2 * target.Level > target.Health + 20) 
                     {
                         spells[Spells.R].Cast(target);
                         Console.WriteLine("Cast R1");
                     }
                 }
 
-                if (GetExecuteDamage(target) > target.Health)
+                if (GetExecuteDamage(target) > target.Health + 20)
                 {
-                    if (IsActive("ElTristana.Combo.Always.R") && GetExecuteDamage(target) > target.Health)
+                    if (IsActive("ElTristana.Combo.Always.R") && GetExecuteDamage(target) > target.Health + 20)
                     {
                         spells[Spells.R].Cast(target);
                     }
 
-                    if (IsECharged(target) && GetExecuteDamage(target) > target.Health)
+                    if (IsECharged(target) && GetExecuteDamage(target) > target.Health + 50)
                     {
                         spells[Spells.R].Cast(target);
                     }
 
-                    if (spells[Spells.R].GetDamage(target) > target.Health || GetExecuteDamage(target) > target.Health)
+                    if (spells[Spells.R].GetDamage(target) > target.Health + 20 || GetExecuteDamage(target) > target.Health + 20)
                     {
                         spells[Spells.R].Cast(target);
                     }
