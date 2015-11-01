643f347abac577cf29a2dad4c175c7f58b5edba6
jQuery <info@zavox.nl>
2015-11-01 16:42:36 +0100

    lux

---
+++ ElLux/ElLux/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ElLux
+{
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Lux.OnLoad;
+        }
+
+        #endregion
+    }
+}
